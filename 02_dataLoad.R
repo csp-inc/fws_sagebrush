5dd9d1d11666bc384a18c07460495a11a1fe38d7
CaitLittlef <caitlin.littlefield@uvm.edu>
2021-10-22 17:46:46 -0400

    resample; boxplot; hist

--- 02_dataLoad.R
+++ 02_dataLoad.R
@@ -1,6 +1,7 @@
 ######################################
 ## DATA COLLECTION & PRE-PROCESSING ##
-#############################################################
+######################################
+
 
 ## Function to load features, set common crs, and fix any invalid geometries
 load_f <- function(f) {
@@ -11,13 +12,11 @@ load_f <- function(f) {
 }
 proj.crs <- "+proj=aea +lat_0=23 +lon_0=-96 +lat_1=29.5 +lat_2=45.5 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"
 
-
-
-
 ## Load AOIs
 biome <- load_f(paste0(data.dir,"US_Sagebrush_Biome_2019.shp"))
 nv <- load_f(paste0(data.dir,"BLM_NV_District_Offices.shp"))
-
+plot(biome)
+plot(nv)
 
 
 ########################################################
@@ -38,70 +37,37 @@ for (i in 2:length(files)){
   stack <- addLayer(stack, raster(files[i]))
 }
 
-# Confirm crs ok?
-isTRUE(print(crs(stack[[1]])) == proj.crs)
+# Confirm crs ok? (asText else get crs object)
+isTRUE(crs(stack[[1]], asText = TRUE) == proj.crs)
+
 
 # Just retain a few for memory
 names(stack) # keep 2010, 2015, 2020
 stack <- stack[[c(2,6,11)]]
 names(stack)
 
+##############################################################
+## Test case of Nevada with coarser data
 # Crop 1 raster to Nevada as test case
+start <- Sys.time()
 nv2020 <- stack[[3]] %>%
   crop(nv) %>%
   mask(nv)
+print(Sys.time() - start) # ~4 min
 
-boxplot(nv2020, nv)
+# Resample to coarser resolution 
+nv2020 <- raster::aggregate(nv2020, fact = 10)
+plot(nv2020)
 
-
-plot(stack$rcmap_sagebrush_2009)
-plot(stack[[1]])
-crs(stack[[1]])
-
-hist(stack[[1]])
-
-
-
-
-hist(stack[[1]])
-
-
-
-stack <- stack(list.rasters[[1]])
-for(i in 2:length(list.rasters)) r <- addLayer(r, stack[[i]])
-plot(r)
-
-
-names(list.rasters[[11]])
-plot(list.rasters[[11]])
-
-
-
-########################################################
-## Test zonal stats  
-# # Turn AOIs into comparable rasters; must be multipolygon
+# Turn Nevada into raster; convert to ok format first
 nv <- st_cast(nv, "MULTIPOLYGON")
-nv.r <- fasterize(nv, stack[[11]])
-# # Error: cannot allocate vector of size 46.5 Gb
+# Raster will need numeric; add and create look-up
+nv$id <- as.numeric(1:6)
+lu.nv <- data.frame(c(1:6),nv$PARENT_NAM) ; colnames(lu.nv) <- c("id","district_name")
+# Set template raster; nb didn't work with higher-res raster -- too much memory
+nv.r <- fasterize(nv, nv2020, field = "id")
 
 
+boxplot(nv2020, nv.r)
+hist(nv2020)
 
-
-zonal.stats(list.rasters[[11]], nv, stats = "mean")
-
-
-list.rasters
-names(list.rasters[[1]])
-zonal.stats(list.rasters[[i]], curr, stats="mean")
-
-
-
-## 
-boo <- st_cast(nv, "MULTIPOLYGON")
-
-foo <- fasterize(boo, list.rasters[[1]])
-
-
-?fasterize
-## Stats by zones
-zonal(curr, poly.r, fun = "mean", na.rm = TRUE)
